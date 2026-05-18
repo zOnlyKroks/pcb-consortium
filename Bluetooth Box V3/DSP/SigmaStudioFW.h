e747a1994f396cd9b124c33dc6447dc1def65e80
go69coq <go69coq@tum.de>
2026-05-18 19:26:39 +0200

    plep

--- Bluetooth Box V3/DSP/SigmaStudioFW.h
+++ Bluetooth Box V3/DSP/SigmaStudioFW.h
@@ -1,4 +1,7 @@
+#include <cstdint>
 #define SIGMA_WRITE_REGISTER_BLOCK SigmaWriteRegisterBlock
+
+#define ADI_REG_TYPE uint8_t
 
 void SigmaWriteRegisterBlock(uint8_t devAddr,
                              uint16_t regAddr,
